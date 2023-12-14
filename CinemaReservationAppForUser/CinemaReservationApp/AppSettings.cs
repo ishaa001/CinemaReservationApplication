b8168b1f96a8213860d65d19f5b9a3216f5e0a6c
ishaa001 <ishaagrawal77@gmail.com>
2023-12-14 23:38:55 +0530

    first commit

---
+++ CinemaReservationAppForUser/CinemaReservationApp/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaReservationApp
+{
+    public static class AppSettings
+    {
+        public static string ApiUrl = "https://cinemareservationapp.azurewebsites.net/";
+    }
+}
---
+++ CinemaReservationAppForUser/CinemaReservationApp/Pages/SearchMoviesPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using CinemaReservationApp.Models;
+using CinemaReservationApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CinemaReservationApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchMoviesPage : ContentPage
+    {
+        public SearchMoviesPage()
+        {
+            InitializeComponent();
+        }
+        private async void EntSearchMovie_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue == null)
+            {
+                return;
+            }
+            var moviesList = await ApiService.FindMovies(e.NewTextValue.ToLower());
+            CvMovies.ItemsSource = moviesList;
+        }
+
+        private void CvMovies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var currentSelection = e.CurrentSelection.FirstOrDefault() as FindMovie;
+            if (currentSelection == null) return;
+            Navigation.PushModalAsync(new MovieDetailPage(currentSelection.Id));
+            ((CollectionView)sender).SelectedItem = null;
+        }
+
+        private void ImgBack_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
